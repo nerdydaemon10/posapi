0250e3d1535446afcf2d8b0452837b4492a1b8d9
keannoregino <keanno.regino@radenta.com>
2025-01-28 17:36:56 +0800

    added success and error response helpers

--- auth/views.py
+++ auth/views.py
@@ -3,7 +3,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from auth.serializers import LoginSerializer
+from core.helpers import error_response
+from .serializers import LoginSerializer
 
 
 # Create your views here.
@@ -12,21 +13,18 @@ class LoginAPIView(APIView):
         serializer = LoginSerializer(data=request.data)
 
         if not serializer.is_valid():
-            return Response(serializer.errors)
+            return error_response(
+                message="Invalid username or password.",
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
 
         username, password = (serializer.data['username'],
                               serializer.data['password'])
         user = authenticate(username=username, password=password)
 
         if user is None:
-            return Response(
-                data={
-                    'isSuccess': False,
-                    'data': None,
-                    'message': 'Invalid username or password',
-                },
-                status=status.HTTP_401_UNAUTHORIZED
+            return error_response(
+                message="Invalid username or password.",
+                status_code=status.HTTP_401_UNAUTHORIZED
             )
-
-        return Response(data={"isSuccess": True, "data": None, "message": "Successfully logged in!"}, status=status.HTTP_200_OK)
 
