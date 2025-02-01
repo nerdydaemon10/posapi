7673dc77726f56e30ba6aa805081bda354d57da5
keannoregino <keanno.regino@radenta.com>
2025-02-01 17:15:33 +0800

    added "RefreshSerializer"

--- auth/serializers.py
+++ auth/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=16, required=True)
     password = serializers.CharField(max_length=16, required=True)
+
+class RefreshSerializer(serializers.Serializer):
+    refresh_token = serializers.CharField(required=True)
+
+    def validate(self, attrs):
+        refresh_token = attrs["refresh_token"]
+        refresh = RefreshToken(refresh_token)
+        data = {
+            "access_token": str(refresh.access_token),
+            "refresh_token": str(refresh)
+        }
+
+        return data
