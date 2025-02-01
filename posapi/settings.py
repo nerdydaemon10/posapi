1d3a766d8ab1a57dba207e8738999172e94d50b5
keannoregino <keanno.regino@radenta.com>
2025-02-01 17:17:35 +0800

    added "rest_framework_simplejwt"

--- posapi/settings.py
+++ posapi/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    )
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
